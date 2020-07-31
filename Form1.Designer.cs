a8f1a8c937bdbbf69d14ca6d9dd222a5665acb7e
SudhirDeshwal <50604228+SudhirDeshwal@users.noreply.github.com>
2020-07-31 03:16:42 -0400

    crud operations done

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,35 +28,189 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtfirstname = new System.Windows.Forms.TextBox();
             this.btnInsert = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtlastname = new System.Windows.Forms.TextBox();
+            this.textstudentid = new System.Windows.Forms.TextBox();
+            this.textsem = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnDisplay = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
-            // textBox1
+            // txtfirstname
             // 
-            this.textBox1.Location = new System.Drawing.Point(94, 100);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(171, 22);
-            this.textBox1.TabIndex = 0;
+            this.txtfirstname.Location = new System.Drawing.Point(158, 44);
+            this.txtfirstname.Name = "txtfirstname";
+            this.txtfirstname.Size = new System.Drawing.Size(171, 22);
+            this.txtfirstname.TabIndex = 0;
             // 
             // btnInsert
             // 
-            this.btnInsert.Location = new System.Drawing.Point(367, 100);
+            this.btnInsert.Location = new System.Drawing.Point(63, 267);
             this.btnInsert.Name = "btnInsert";
-            this.btnInsert.Size = new System.Drawing.Size(75, 23);
+            this.btnInsert.Size = new System.Drawing.Size(89, 32);
             this.btnInsert.TabIndex = 1;
             this.btnInsert.Text = "Insert";
             this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(60, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "FirstName";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(60, 162);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "StudentId";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(60, 213);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Semester";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(60, 93);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 17);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "LastName";
+            // 
+            // txtlastname
+            // 
+            this.txtlastname.Location = new System.Drawing.Point(158, 93);
+            this.txtlastname.Name = "txtlastname";
+            this.txtlastname.Size = new System.Drawing.Size(171, 22);
+            this.txtlastname.TabIndex = 6;
+            // 
+            // textstudentid
+            // 
+            this.textstudentid.Location = new System.Drawing.Point(158, 162);
+            this.textstudentid.Name = "textstudentid";
+            this.textstudentid.Size = new System.Drawing.Size(171, 22);
+            this.textstudentid.TabIndex = 7;
+            // 
+            // textsem
+            // 
+            this.textsem.Location = new System.Drawing.Point(158, 213);
+            this.textsem.Name = "textsem";
+            this.textsem.Size = new System.Drawing.Size(171, 22);
+            this.textsem.TabIndex = 8;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 373);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(603, 189);
+            this.dataGridView1.TabIndex = 9;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(194, 266);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(93, 33);
+            this.btnUpdate.TabIndex = 10;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(333, 266);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(83, 33);
+            this.btnDelete.TabIndex = 11;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnDisplay
+            // 
+            this.btnDisplay.Location = new System.Drawing.Point(475, 266);
+            this.btnDisplay.Name = "btnDisplay";
+            this.btnDisplay.Size = new System.Drawing.Size(80, 33);
+            this.btnDisplay.TabIndex = 12;
+            this.btnDisplay.Text = "Display";
+            this.btnDisplay.UseVisualStyleBackColor = true;
+            this.btnDisplay.Click += new System.EventHandler(this.btnDisplay_Click);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(63, 589);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(192, 22);
+            this.txtSearch.TabIndex = 13;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(299, 580);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(81, 41);
+            this.btnSearch.TabIndex = 14;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(390, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(135, 28);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Details By Name";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(647, 641);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.btnDisplay);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textsem);
+            this.Controls.Add(this.textstudentid);
+            this.Controls.Add(this.txtlastname);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnInsert);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtfirstname);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -64,8 +218,22 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtfirstname;
         private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtlastname;
+        private System.Windows.Forms.TextBox textstudentid;
+        private System.Windows.Forms.TextBox textsem;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnDisplay;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,14 +7,108 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace SQl_WindowForm_Sudhir_n01324321
 {
     public partial class Form1 : Form
     {
+
+        SqlConnection con = new SqlConnection("Data Source=LAPTOP-CCPDKVPS\\SQLEXPRESS;Initial Catalog=demo;Integrated Security=True");
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            //  SqlCommand cmd = new SqlCommand("insert into Students(FirstName) values('"+textBox1.Text+"')", con);
+            // con.Open();
+            // int i = cmd.ExecuteNonQuery();
+            // if (i != 0)
+            // {
+            //     MessageBox.Show("Data Inserted");
+            // }
+            // else
+            // {
+            //     MessageBox.Show("Error");
+
+            // }
+
+
+
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "insert into [Students] (FirstName , LastName , StudentId , Semester) values('" + txtfirstname.Text + "' ,'" + txtlastname.Text + "', '" + textstudentid.Text + "','" + textsem.Text + "')";
+            cmd.ExecuteNonQuery();
+            con.Close();
+            Displaydata();
+            MessageBox.Show("Data Inserted");
+
+        }
+
+          
+        public void Displaydata()
+        {
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "Select * from [Students]";
+            cmd.ExecuteNonQuery();
+
+            DataTable table = new DataTable();
+            SqlDataAdapter dataadp = new SqlDataAdapter(cmd);
+            dataadp.Fill(table);
+            dataGridView1.DataSource = table;
+            con.Close();
+
+        }
+
+        private void btnDisplay_Click(object sender, EventArgs e)
+        {
+            Displaydata();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "Delete from [Students] where FirstName = '" + txtfirstname.Text + "' ";
+            cmd.ExecuteNonQuery();
+            con.Close();
+            Displaydata();
+            MessageBox.Show("Data Removed");
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "UPDATE [Students] SET LastName = '"+txtlastname.Text+ "' ,StudentId = '" + textstudentid.Text + "' , Semester = '" + textsem.Text + "'  where FirstName = '" + txtfirstname.Text + "' ";
+            cmd.ExecuteNonQuery();
+            con.Close();
+            Displaydata();
+            MessageBox.Show("Data Updated");
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+
+
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "Select * from [Students] where FirstName = '" + txtSearch.Text + "' ";
+            cmd.ExecuteNonQuery();
+            DataTable table = new DataTable();
+            SqlDataAdapter dataadp = new SqlDataAdapter(cmd);
+            dataadp.Fill(table);
+            dataGridView1.DataSource = table;
+            con.Close();
         }
     }
 }
